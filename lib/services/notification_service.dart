ed21c91a39d96539fe16246f8c8d7ec482543efe
TY <rcty666@gmail.com>
2026-03-24 01:08:03 +0800

    license update

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,4 +1,4 @@
-// Copyright © 2026 TY Chew. All rights reserved.
+// Copyright © 2026 TY Chew, Jimmy Kee. All rights reserved.
 // Licensed under the MIT License. See LICENSE file in the project root.
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
